e64ed90d8ec49fdb87dbaba54718a9741b4ca437
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-20 15:51:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(input) {
+  let block =  input;
+  blockNumber = Math.abs(input - 42);
+  return blockNumber;
+}
+
+function distanceFromHqInFeet(input) {
+  let block = input;
+  blockNumber = Math.abs(input -42)*264;
+  return blockNumber;
+}
+
+function distanceTravelledInFeet (input1, input2) {
+  let block1 = input1;
+  let block2 = input2;
+  betweenBlocks = Math.abs(input1 - input2)*264;
+  return betweenBlocks;
+}
+
+function calculatesFarePrice(input1, input2) {
+  let cost;
+  calculatedFeet = distanceTravelledInFeet(input1, input2);
+  if (calculatedFeet <= 400){
+      cost = 0;
+  }
+  else if (calculatedFeet > 400 && calculatedFeet<=2000){
+    cost = 0.02 * Math.abs(calculatedFeet - 400);
+  }
+  else if (calculatedFeet > 2000 && calculatedFeet < 2500) {
+    cost = 25
+  }
+  else if (calculatedFeet > 2500) {
+    return "cannot travel that far";
+  }
+  return cost;
+}
