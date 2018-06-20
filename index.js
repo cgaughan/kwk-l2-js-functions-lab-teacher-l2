5bb0c5abfefbd5357f746cf9a39a8006fe105e6a
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-20 15:45:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,15 +25,6 @@ function calculatesFarePrice(start, destination) {
   }
 
 
- {
 
-  cost = 0
-  }
-  else if (distanceTraveled > 2000 && distanceTraveled < 2500) {
-    return "I will gladly take your thirty bucks."
-  }
-  else if (distanceTraveled > 2500) {
-    return "No can do."
-  }
 }
 }
